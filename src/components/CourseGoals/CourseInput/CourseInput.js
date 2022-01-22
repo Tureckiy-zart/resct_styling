82eb5d785a9a5f3b2a9b3f81c276e3fae5d377fe
Yevhen  (Jay) Zhdanov <yevhen.zhdanov@3ss.tv>
2022-01-22 20:38:44 +0200

    some fixes

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = ({ onAddGoal }) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim()) {
+      setIsValid(true);
+      setEnteredValue(event.target.value.trim());
+    }
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (!enteredValue.length) {
+      setIsValid(false);
+      return;
+    }
+    onAddGoal(enteredValue);
+    setEnteredValue("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label htmlFor="input">Course Goal</label>
+        <input
+        id='input'
+          type="text"
+          onChange={goalInputChangeHandler}
+          value={enteredValue}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css';
+import "./Button.css";
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = ({ type, onClick, children }) => (
+  <button type={type} className="button" onClick={onClick}>
+    {children}
+  </button>
+);
 
 export default Button;
